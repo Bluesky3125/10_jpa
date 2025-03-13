e61b2990586655f159664200e3bed21cf6f3c84b
Bluesky3125 <charlie4822@gmail.com>
2025-03-13 12:47:36 +0900

    25/03/13 12:50
    jpa chap01 update까지

---
+++ chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section02/crud/EntityManagerCRUDTest.java
@@ -0,0 +1,88 @@
+package com.ohgiraffers.section02.crud;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class EntityManagerCRUDTest {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initEntityManager() {
+
+        /* 설명. EntityManager가 생성될 때마다 고유의 새로운 영속성 컨텍스트(Entity를 관리하는 Java의 창고)가 생성된다. */
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void 메뉴코드로_메뉴_조회_테스트() {
+
+        int menuCode = 2;
+
+        /* 설명. entityManager를 통해 여러 번 find를 해도 select는 한 번만 동작한다. */
+        // find(엔티티, PK 속성), 복잡한 조회는 직접 쿼리문을 작성해야 함
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+        Menu foundMenu2 = entityManager.find(Menu.class, menuCode);
+        Menu foundMenu3 = entityManager.find(Menu.class, menuCode);
+
+        /* 설명. 단정문은 두 개 이상도 가능 */
+        Assertions.assertNotNull(foundMenu);
+        Assertions.assertEquals(menuCode, foundMenu.getMenuCode());
+        System.out.println("foundMenu = " + foundMenu);
+    }
+
+    @Test
+    public void 새로운_메뉴_추가_테스트() {
+        Menu menu = new Menu();
+        menu.setMenuName("꿀발린추어탕");
+        menu.setMenuPrice(7000);
+        menu.setCategoryCode(4);
+        menu.setOrderableStatus("Y");
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();    // @Transactional로 대체될 예정
+
+        try {
+            entityManager.persist(menu);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+    }
+
+    @Test
+    public void 메뉴_이름_수정_테스트() {
+        Menu menu = entityManager.find(Menu.class, 2);  // 없으면 select로 가져옴
+        System.out.println("menu = " + menu);
+
+        String menuNameToChange = "갈치스무디";
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            menu.setMenuName(menuNameToChange); // 스냅샷과 다르면 update
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+    }
+}
---
+++ chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section02/crud/Menu.java
@@ -0,0 +1,95 @@
+package com.ohgiraffers.section02.crud;
+
+import jakarta.persistence.*;
+
+@Entity(name="section02_menu")
+@Table(name="tbl_menu")
+public class Menu { // 기본 엔티티 명: menu
+
+    @Id
+    /* 설명. 복합키 설정 방법은 나중에 설명 */
+    @Column(name="menu_code")   // resultMap(기본값 snake_case)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    /* 설명. DBMS에 따라 다른 번호 발생 방식
+    *   TABLE: 번호 발생 테이블을 생성하여 번호 관리(거의 대부분 사용 가능)
+    *   SEQUENCE: Oracle 같은 곳에서 사용
+    *   IDENTITY: auto_increment 활용
+    *   UUID: 난수
+    *   AUTO: DBMS에 맞추어 자동 설정
+    * */
+    private int menuCode;
+
+    @Column(name="menu_name")
+    private String menuName;
+
+    @Column(name="menu_price")
+    private int menuPrice;
+
+    @Column(name="category_code")
+    private int categoryCode;
+
+    @Column(name="orderable_status")
+    private String orderableStatus;
+
+    public Menu() {
+    }
+
+    public Menu(int menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public void setMenuCode(int menuCode) {
+        this.menuCode = menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
